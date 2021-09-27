95713e5e54715c7a94aeb095016da723c5aadb92
Ebin Abraham Varghese <ebin.varghese@kgisl.com>
2021-09-27 18:26:11 +0530

    Adding DbContext Class file

---
+++ FirstApi/DataModel/StudentAdminContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApi.DataModel
+{
+    public class StudentAdminContext : DbContext
+    {
+        public  StudentAdminContext(DbContextOptions<StudentAdminContext> options):base(options)
+        {
+
+        }
+        public DbSet<Address> Address { get; set; }
+
+        public DbSet<Gender> Gender { get; set; }
+
+        public DbSet<Student> Student { get; set; }
+
+    }
+}
