0fda3abf964b172b6c1036266e590bb70572476a
Ebin Abraham Varghese <ebin.varghese@kgisl.com>
2021-09-27 17:58:41 +0530

    Added Model class in this solution

---
+++ FirstApi/DataModel/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApi.DataModel
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+
+        public string PostalAddress { get; set; }
+
+        public string PhysicalAddress { get; set; }
+
+        public Guid StudentId { get; set; }
+
+
+    }
+}
